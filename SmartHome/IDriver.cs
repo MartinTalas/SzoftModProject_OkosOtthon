6b304b8057f3780f8f2e988661492a01398d2051
Macska kapitany <42695936+mcmacska@users.noreply.github.com>
2021-04-19 20:13:26 +0200

    új smarthome core

---
+++ SmartHome/IDriver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeProject
+{
+    interface IDriver
+    {
+        int sendCommand(Subscriber subs, bool boiler, bool ac);
+    }
+}
---
+++ SmartHome/bin/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            Monitor m = new Monitor();
+            m.monitor_main();
+            Console.ReadKey();
+        }
+    }
+}
