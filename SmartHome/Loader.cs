83a15e1fb929c2777fc0aef589ad2f2851260b5c
MartinTalas <70484866+MartinTalas@users.noreply.github.com>
2021-04-19 20:46:56 +0200

    Loader class / ILoader Interface
    ILoader interface megcsinálva
    public Subscribers loadSubscribers();
    
    Loader osztály implementálja az ILoadert
    
    -> implementált függvény meghívja a beolvalsót, amely belolvas json-ből, majd return value-nak visszaadja a subscribers objektumot.
    
    a beolvasáshoz -> using System.Text.Json;
    beolvasó: private void loadFromJson();
    
    Filename: subscriberdata.json

---
+++ SmartHome/Loader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace SmartHome
+{
+    class Loader : ILoader
+    {
+        private const string file_name = "subscriberdata.json";
+        private Subscribers loaded_subscribers = new Subscribers();
+        public Subscribers loadSubscribers()
+        {
+            this.loadFromJson();
+            return this.loaded_subscribers;
+        }
+
+        private void loadFromJson()
+        {
+            string jsonString = File.ReadAllText(file_name);
+            loaded_subscribers = JsonSerializer.Deserialize<Subscribers>(jsonString);
+        }
+    }
+}
--- SmartHome/Program.cs
+++ SmartHome/Program.cs
@@ -6,7 +6,11 @@ namespace SmartHome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Loader debug_loader = new Loader();
+            Subscribers debug_subscribers = new Subscribers();
+            debug_subscribers = debug_loader.loadSubscribers();
+            Console.WriteLine(debug_subscribers.subscribers.Count);
+            Console.WriteLine(debug_subscribers.subscribers[0].homeId);
         }
     }
 }
