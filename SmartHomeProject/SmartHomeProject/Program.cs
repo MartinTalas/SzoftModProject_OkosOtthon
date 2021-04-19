590d157c232a6612bfe76aac2f0140f7269ff5be
MartinTalas <70484866+MartinTalas@users.noreply.github.com>
2021-04-19 17:10:27 +0200

    First Steps

---
+++ SmartHomeProject/SmartHomeProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Uraim, megbaszódunk");
+            Console.ReadKey();
+        }
+    }
+}
