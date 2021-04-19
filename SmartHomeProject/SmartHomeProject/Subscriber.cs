a7402f83cb91e976c3f470c0787d5940ecf22307
Macska kapitany <42695936+mcmacska@users.noreply.github.com>
2021-04-19 18:01:33 +0200

    osztályok
    Subscriber
    Subscribers
    Temperature

---
+++ SmartHomeProject/SmartHomeProject/Subscriber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHomeProject
+{
+    class Subscriber
+    {
+        public string subscriber
+        {
+            get;
+            set;
+        }
+        public string homeId
+        {
+            get;
+            set;
+        }
+        public string boilerType
+        {
+            get;
+            set;
+        }
+        public string airConditionerType
+        {
+            get;
+            set;
+        }
+        public List<Temperature> temperatures
+        {
+            get;
+            set;
+        }
+    }
+}
