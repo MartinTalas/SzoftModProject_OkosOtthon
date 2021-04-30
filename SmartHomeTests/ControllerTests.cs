e2a5504f58dc47f488ba496cb338193d021f2af8
AdamBeer2000 <57035872+AdamBeer2000@users.noreply.github.com>
2021-04-30 19:54:07 +0200

    Moudle Testek

---
+++ SmartHomeTests/ControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartHome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartHome.Tests
+{
+    [TestClass()]
+    public class ControllerTests
+    {
+        [TestMethod()]
+        public void ControllerTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
